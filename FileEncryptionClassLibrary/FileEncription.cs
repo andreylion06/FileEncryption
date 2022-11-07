e250561e15fb787ceb7dc8dffbde8595071eee0c
Andrii Sakhno <andreylion.zt@gmail.com>
2022-11-07 22:39:06 +0200

    Created FileEncryptionClassLibrary. The Main Encryption Logic was Implemented

---
+++ FileEncryptionClassLibrary/FileEncription.cs
@@ -0,0 +1,44 @@
+﻿namespace FileEncryptionClassLibrary
+{
+    public class FileEncription
+    {
+        private const string FileCryptExtension = ".crypt";
+        private const string FileDecryptExtension = ".decrypt";
+
+        private string Password { get; set; }
+        private string FileInPath { get; set; }
+        public FileEncription(string password, string fileInPath)
+        {
+            Password = password;
+            FileInPath = fileInPath;
+        }
+
+        public void CryptOrUncrypt()
+        {
+            string fileOutPath = GetOutPath(FileInPath);
+
+            Console.WriteLine(fileOutPath);
+            FileStream fileStreamIn = File.OpenRead(FileInPath);
+            FileStream fileStreamOut = File.OpenWrite(fileOutPath);
+            int symbol;
+            int posPassword = 0;
+            while ((symbol = fileStreamIn.ReadByte()) != -1)
+            {
+                symbol = symbol ^ Password[posPassword++];
+                fileStreamOut.WriteByte((byte)symbol);
+                if (posPassword >= Password.Length)
+                    posPassword = 0;
+            }
+            fileStreamIn.Close();
+            fileStreamOut.Close();
+        }
+
+        private string GetOutPath(string fileInPath)
+        {
+            if (fileInPath.Contains(FileCryptExtension))
+                return fileInPath.Remove(fileInPath.IndexOf(FileCryptExtension)) + FileDecryptExtension;
+            else
+                return fileInPath + FileCryptExtension;
+        }
+    }
+}
